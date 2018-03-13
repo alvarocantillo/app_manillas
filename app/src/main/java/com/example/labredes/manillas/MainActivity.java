c566cdb7696e439706c8b9de779cf04e5c9af5ad
Alvaro_Cantillo <alvjc225@gmail.com>
2018-03-13 16:07:03 -0500

    Ya cotiza

---
+++ app/src/main/java/com/example/labredes/manillas/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.labredes.manillas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+public class MainActivity extends AppCompatActivity {
+    private RadioButton cuerdas;
+    private RadioButton cuero;
+    private RadioButton martillo;
+    private RadioButton ancla;
+    private RadioButton oro;
+    private RadioButton plata;
+    private RadioButton niquel;
+    private RadioButton usd;
+    private RadioButton cop;
+    private Button total;
+    private EditText num;
+    private EditText valor;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        cuerdas= findViewById(R.id.scuerda);
+        cuero= findViewById(R.id.scuero);
+        martillo= findViewById(R.id.smartillo);
+        ancla= findViewById(R.id.sancla);
+        oro=findViewById(R.id.soro);
+        plata=findViewById(R.id.splata);
+        niquel=findViewById(R.id.sniquel);
+        usd=findViewById(R.id.susd);
+        cop= findViewById(R.id.scop);
+        total=findViewById(R.id.stotal);
+        num=findViewById(R.id.txtcantidad);
+        valor=findViewById(R.id.txtresultado);
+
+
+
+    }
+    public double cotizar() {
+        double resultado = 0;
+        if (cuero.isChecked()&&martillo.isChecked()&&oro.isChecked()){
+            resultado = 100;
+            return resultado;
+        }else  if (cuero.isChecked()&&martillo.isChecked()&&plata.isChecked()){
+            resultado = 80;
+            return resultado;
+        }else  if (cuero.isChecked()&&martillo.isChecked()&&niquel.isChecked()) {
+            resultado = 70;
+            return resultado;
+        }else  if (cuero.isChecked()&&ancla.isChecked()&&oro.isChecked()) {
+            resultado = 120;
+            return resultado;
+        }else  if (cuero.isChecked()&&ancla.isChecked()&&plata.isChecked()) {
+            resultado = 100;
+            return resultado;
+        }
+        else  if (cuero.isChecked()&&ancla.isChecked()&&niquel.isChecked()) {
+            resultado = 90;
+            return resultado;
+        }else  if (cuerdas.isChecked()&&martillo.isChecked()&&oro.isChecked()) {
+            resultado = 90;
+            return resultado;
+        }else  if (cuerdas.isChecked()&&martillo.isChecked()&&plata.isChecked()) {
+            resultado = 70;
+            return resultado;
+        }else  if (cuerdas.isChecked()&&martillo.isChecked()&&niquel.isChecked()) {
+            resultado = 50;
+            return resultado;
+        }else  if (cuerdas.isChecked()&&ancla.isChecked()&&oro.isChecked()) {
+            resultado = 110;
+            return resultado;
+        }else  if (cuerdas.isChecked()&&ancla.isChecked()&&plata.isChecked()) {
+            resultado = 90;
+            return resultado;
+        }else  if (cuerdas.isChecked()&&ancla.isChecked()&&niquel.isChecked()) {
+            resultado = 50;
+            return resultado;
+        }
+        {
+            return resultado;
+        }
+    }
+
+
+    public void calcular(View V){
+        double seleccion= cotizar();
+        int canti= Integer.parseInt(num.getText().toString());
+        seleccion=seleccion*canti;
+        valor.setText(seleccion+"");
+    }
+    }
+
---
+++ app/src/main/java/com/example/labredes/manillas/Metodos.java
@@ -0,0 +1,8 @@
+package com.example.labredes.manillas;
+
+/**
+ * Created by Lab Redes on 13/03/2018.
+ */
+
+public class Metodos {
+}
